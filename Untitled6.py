65b88c6d0640ed1a1cff291a22e0b71659bc70d3
Mohammadsprojects <58224977+Mohammadsprojects@users.noreply.github.com>
2019-11-26 21:27:58 +0530

    Add files via upload

---
+++ Untitled6.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# # Coursera_Capstone Project
+
+# In[1]:
+
+
+# imports
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+# print
+print("Hello Capstone Project Course!")
+
